57afe9c0ca8e7f483c2086bc1817f889b7130f46
HunterX-7 <fraquin7@gmail.com>
2023-01-18 16:41:48 -0500

    Refactor to use class in App.js instead of functions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import React from 'react';
 import './App.css';
 import Calculator from './components/calculator';
 
-const App = () => (
-  <div className="App mt-5 text-center">
-    <h1>Magic Math Calculator</h1>
-    <Calculator />
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App mt-5 text-center">
+        <h1>Magic Math Calculator</h1>
+        <Calculator />
+      </div>
+    );
+  }
+}
 
 export default App;
