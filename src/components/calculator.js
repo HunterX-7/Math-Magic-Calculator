8058869cde60a8cff35333dfd682366999f5e705
HunterX-7 <fraquin7@gmail.com>
2023-01-20 01:02:24 -0500

    Refactor calculator.js to use functions instead of classes

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,23 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import calculate from '../logic/calculate';
 
-export default class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-    this.clickEvent = this.clickEvent.bind(this);
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  })
+
+  const clickEvent = (e) => {
+    setState((state) => calculate(state, e.target.innerText));
   }
 
-  clickEvent(e) {
-    this.setState((state) => calculate(state, e.target.innerText));
-  }
+    const { total, next, operation } = state;
 
-  render() {
-    const { total, next, operation } = this.state;
     return (
       <section>
         <div className="container mt-4 calc-container">
@@ -29,36 +25,37 @@ export default class Calculator extends React.Component {
             </p>
           </div>
           <div className="d-flex">
-            <button onClick={this.clickEvent} type="button" className="btn btn-secondary btn-custom">AC</button>
-            <button onClick={this.clickEvent} type="button" className="btn btn-secondary btn-custom">+/-</button>
-            <button onClick={this.clickEvent} type="button" className="btn btn-secondary btn-custom">%</button>
-            <button onClick={this.clickEvent} type="button" className="btn btn-warning btn-custom2">÷</button>
+            <button onClick={clickEvent} type="button" className="btn btn-secondary btn-custom">AC</button>
+            <button onClick={clickEvent} type="button" className="btn btn-secondary btn-custom">+/-</button>
+            <button onClick={clickEvent} type="button" className="btn btn-secondary btn-custom">%</button>
+            <button onClick={clickEvent} type="button" className="btn btn-warning btn-custom2">÷</button>
           </div>
           <div className="d-flex">
-            <button onClick={this.clickEvent} type="button" className="btn btn-secondary btn-custom">7</button>
-            <button onClick={this.clickEvent} type="button" className="btn btn-secondary btn-custom">8</button>
-            <button onClick={this.clickEvent} type="button" className="btn btn-secondary btn-custom">9</button>
-            <button onClick={this.clickEvent} type="button" className="btn btn-warning btn-custom2">x</button>
+            <button onClick={clickEvent} type="button" className="btn btn-secondary btn-custom">7</button>
+            <button onClick={clickEvent} type="button" className="btn btn-secondary btn-custom">8</button>
+            <button onClick={clickEvent} type="button" className="btn btn-secondary btn-custom">9</button>
+            <button onClick={clickEvent} type="button" className="btn btn-warning btn-custom2">x</button>
           </div>
           <div className="d-flex">
-            <button onClick={this.clickEvent} type="button" className="btn btn-secondary btn-custom">4</button>
-            <button onClick={this.clickEvent} type="button" className="btn btn-secondary btn-custom">5</button>
-            <button onClick={this.clickEvent} type="button" className="btn btn-secondary btn-custom">6</button>
-            <button onClick={this.clickEvent} type="button" className="btn btn-warning btn-custom2">-</button>
+            <button onClick={clickEvent} type="button" className="btn btn-secondary btn-custom">4</button>
+            <button onClick={clickEvent} type="button" className="btn btn-secondary btn-custom">5</button>
+            <button onClick={clickEvent} type="button" className="btn btn-secondary btn-custom">6</button>
+            <button onClick={clickEvent} type="button" className="btn btn-warning btn-custom2">-</button>
           </div>
           <div className="d-flex">
-            <button onClick={this.clickEvent} type="button" className="btn btn-secondary btn-custom">1</button>
-            <button onClick={this.clickEvent} type="button" className="btn btn-secondary btn-custom">2</button>
-            <button onClick={this.clickEvent} type="button" className="btn btn-secondary btn-custom">3</button>
-            <button onClick={this.clickEvent} type="button" className="btn btn-warning btn-custom2">+</button>
+            <button onClick={clickEvent} type="button" className="btn btn-secondary btn-custom">1</button>
+            <button onClick={clickEvent} type="button" className="btn btn-secondary btn-custom">2</button>
+            <button onClick={clickEvent} type="button" className="btn btn-secondary btn-custom">3</button>
+            <button onClick={clickEvent} type="button" className="btn btn-warning btn-custom2">+</button>
           </div>
           <div className="d-flex">
-            <button onClick={this.clickEvent} type="button" className="btn btn-secondary btn-custom3">0</button>
-            <button onClick={this.clickEvent} type="button" className="btn btn-secondary btn-custom">.</button>
-            <button onClick={this.clickEvent} type="button" className="btn btn-warning btn-custom2">=</button>
+            <button onClick={clickEvent} type="button" className="btn btn-secondary btn-custom3">0</button>
+            <button onClick={clickEvent} type="button" className="btn btn-secondary btn-custom">.</button>
+            <button onClick={clickEvent} type="button" className="btn btn-warning btn-custom2">=</button>
           </div>
         </div>
       </section>
     );
-  }
 }
+
+export default Calculator;
